3ec70a2bf41262108a655949ac32eb35d1f2862e
Yi Kuo <kuokuoyiyi@gmail.com>
2021-09-10 18:55:36 +0800

    feat(app): Allow empty error message

--- lib/src/widgets/login_card.dart
+++ lib/src/widgets/login_card.dart
@@ -190,8 +190,10 @@ class _LoginCardState extends State<_LoginCard> with TickerProviderStateMixin {
 
     await _submitController.reverse();
 
-    if (!DartHelper.isNullOrEmpty(error)) {
-      showErrorToast(context, messages.flushbarTitleError, error!);
+    if (error != null) {
+      if (error.isNotEmpty) {
+        showErrorToast(context, messages.flushbarTitleError, error);
+      }
       Future.delayed(const Duration(milliseconds: 271), () {
         setState(() => _showShadow = true);
       });
