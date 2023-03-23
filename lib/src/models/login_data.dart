0c4cb5fc1a573b7f3927c02252ad7c69b66fc4f5
Yi Kuo <kuokuoyiyi@gmail.com>
2023-03-23 10:31:42 +0800

    feat: field for legal name

--- lib/src/models/login_data.dart
+++ lib/src/models/login_data.dart
@@ -4,22 +4,27 @@ class LoginData {
   final String name;
   final String password;
   final String? email;
+  final String? legalName;
 
-  LoginData({required this.name, required this.password, this.email});
+  LoginData(
+      {required this.name, required this.password, this.email, this.legalName});
 
   @override
   String toString() {
-    return '$runtimeType($name, $password)';
+    return '$runtimeType($name, $password, $email, $legalName)';
   }
 
   @override
   bool operator ==(Object other) {
     if (other is LoginData) {
-      return name == other.name && password == other.password;
+      return name == other.name &&
+          password == other.password &&
+          email == other.email &&
+          legalName == other.legalName;
     }
     return false;
   }
 
   @override
-  int get hashCode => hash2(name, password);
+  int get hashCode => hash4(name, password, email, legalName);
 }
